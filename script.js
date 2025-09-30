33683eb73f9bfab6734f5ea15ea0408a06fcb596
Dani820-ai <ramdaniiii554@gmail.com>
2025-09-30 16:25:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function calculate() {
     
     // This will replace the result of any mathematical operation with "I love you"
     if (expression.match(/[0-9]+[+\-*/][0-9]+/)) {
-        display.value = 'Aku Gilaa';
+        display.value = '😹😹😹😹😹😹😹';
     } else {
         try {
             display.value = eval(expression);
@@ -23,5 +23,6 @@ function clearDisplay() {
     const display = document.getElementById('display');
     display.value = '';
 }
+
 
 
